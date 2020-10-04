2bf5dc3231926c008ba8aec02dae556675ff146d
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-10-04 22:27:46 +0200

    dirname

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express'); 
 const app = express(); 
 const port = process.env.PORT || 1337;
+const path = require('path')
 const bodyParser = require('body-parser');
 const { getAllBoats, getBoat, addBoat, deleteBoat, search, getOnlyFive } = require('./database.js');
 
