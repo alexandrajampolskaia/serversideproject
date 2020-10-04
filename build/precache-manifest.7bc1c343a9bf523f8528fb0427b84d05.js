4aa8b1db37fec05c2c88c4bfad62169f03220d37
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-10-04 21:50:38 +0200

    added build map

---
+++ build/precache-manifest.7bc1c343a9bf523f8528fb0427b84d05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee14254f9439c32d4c75aaf3bdd4043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcd5ca9cbca303dbe380",
+    "url": "/static/css/main.52491ac9.chunk.css"
+  },
+  {
+    "revision": "1065922cbfba5bd8ef2b",
+    "url": "/static/js/2.1a01c43b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a01c43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd5ca9cbca303dbe380",
+    "url": "/static/js/main.f5acfc13.chunk.js"
+  },
+  {
+    "revision": "d451bc63b56136079981",
+    "url": "/static/js/runtime-main.230ed7ae.js"
+  }
+]);
